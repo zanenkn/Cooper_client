50fc7fe73ba7cbe4b98c5a0145adfc499fe406e2
Carrosen <carla1.rosen@gmail.com>
2019-05-15 21:58:30 +0200

    Modifies the render function in the App component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import DisplayCooperResult from './Components/DisplayCooperResult'
 import InputFields from './Components/InputFields'
 import LoginForm from './Components/LoginForm'
+import { authenticate } from './Modules/Auth'
 
 class App extends Component {
   constructor(props) {
@@ -35,19 +36,32 @@ class App extends Component {
   }
 
   render() {
-    let renderLogin
+    let renderLogin;
+    let user;
 
-    if (this.state.renderLoginForm === true) {
+    if (this.state.authenticated === true) {
+      user = JSON.parse(sessionStorage.getItem('credentials')).uid
       renderLogin = (
-        <LoginForm 
-          loginHandler={this.onLogin.bind(this)}
-          inputChangeHandler={this.onChange.bind(this)}
-        />
+        <p>Hi {user}</p>
       )
     } else {
-      renderLogin = (
-        <button id="login" onClick={() => this.setState({ renderLoginForm: true})}>Login</button>
-      )
+      if (this.state.renderLoginForm === true) {
+        renderLogin = (
+          <>
+            <LoginForm 
+              loginHandler={this.onLogin.bind(this)}
+              inputChangeHandler={this.onChange.bind(this)}
+            />
+          </>
+        )
+      } else {
+        renderLogin = (
+          <>
+            <button id="login" onClick={() => this.setState({ renderLoginForm: true})}>Login</button>
+            <p>{this.state.message}</p>
+          </>
+        )
+      }
     }
 
     return (
