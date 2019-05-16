1abc6d06d1b69e107abce80458a8b48810fcfe0e
zanenkn <zane.neikena@gmail.com>
2019-05-16 10:41:26 +0200

    Fixes the login.json, tests are now passing. Adds another feature test - 'user can save performance data'.

---
+++ cypress/integration/userCanSavePerformanceData.spec.js
@@ -0,0 +1,33 @@
+describe('User attempts save data', () => {
+
+  beforeEach(function() {
+    cy.visit('http://localhost:3001');
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/auth/sign_in',
+      response: 'fixture:login.json',
+      headers: {
+        "uid": "user@mail.com" 
+      }
+    })
+    cy.get('#login').click();
+    cy.get('#login-form').within(() => {
+      cy.get('#email').type('user@mail.com')
+      cy.get('#password').type('password')
+      cy.get('button').click()
+    })
+  });
+
+  it('successfully', () => {
+    cy.get('input[id="distance"]').type('1000')
+    cy.get('select[id="gender"]').select('female')
+    cy.get('input[id="age"]').type('23')
+    cy.get('#save-result').click()
+    cy.contains('Your entry waas saved')
+    cy.get('input[id="distance"]')
+      .clear()
+      .type('1500')
+    cy.get('#save-result').click()
+    cy.contains('Your entry was saved')    
+  })
+})
