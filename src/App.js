85130996748fdac765837d609ca29cc90b95140e
zanenkn <zane.neikena@gmail.com>
2019-05-16 14:09:00 +0200

    Adds button 'Show past entries' to the App component, creates the DisplayPerformanceData component, imports it to the App

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import DisplayCooperResult from './Components/DisplayCooperResult'
 import InputFields from './Components/InputFields.jsx'
 import LoginForm from './Components/LoginForm'
 import { authenticate } from './Modules/Auth'
+import DisplayPerformanceData from './Components/DisplayPerformanceData'
 
 class App extends Component {
   constructor(props) {
@@ -16,7 +17,8 @@ class App extends Component {
       email: '',
       password: '',
       message: '',
-      entrySaved: false
+      entrySaved: false,
+      renderIndex: false
     }
   }
 
@@ -44,11 +46,15 @@ class App extends Component {
   render() {
     let renderLogin;
     let user;
+    let performanceDataIndex;
 
     if (this.state.authenticated === true) {
       user = JSON.parse(sessionStorage.getItem('credentials')).uid
       renderLogin = (
         <p>Hi {user}</p>
+      )
+      performanceDataIndex = (
+        <button id="show-index" onClick={() => this.setState({ renderIndex: true })}>Show past entries</button>
       )
     } else {
       if (this.state.renderLoginForm === true) {
@@ -84,6 +90,7 @@ class App extends Component {
           entrySaved={this.state.entrySaved}
           entryHandler={this.entryHandler.bind(this)}
         />
+        {performanceDataIndex}
         {renderLogin}
       </>
     );
