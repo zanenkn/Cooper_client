c642b88b2c8fe30f5a4fbdf6297789789d04d95c
Carrosen <carla1.rosen@gmail.com>
2019-05-16 20:55:41 +0200

    Adds a fixture for testing 'User can sign up', adjust the spec accordingly

--- cypress/integration/userCanSingUp.spec.js
+++ cypress/integration/userCanSingUp.spec.js
@@ -1,6 +1,15 @@
 describe('User can sign up', () => {
   it('successfully', () => {
     cy.visit('http://localhost:3001');
+    cy.server()
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/auth',
+      response: 'fixture:signup.json',
+      headers: {
+        "uid": "another@mail.com"
+      }
+    })
     cy.get('#signup').click();
     cy.get('#signup-form').within(() => {
       cy.get('#email').type('another@mail.com')
@@ -13,6 +22,18 @@ describe('User can sign up', () => {
 
   it('with invalid credentials - invalid email', () => {
     cy.visit('http://localhost:3001');
+    cy.server()
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/auth',
+      status: "401",
+      response: {
+        "errors": [
+          "Invalid signup credentials. Please try again."
+        ],
+        "success":false
+      }
+    })
     cy.get('#signup').click();
     cy.get('#signup-form').within(() => {
       cy.get('#email').type('another@mail')
@@ -20,11 +41,23 @@ describe('User can sign up', () => {
       cy.get('#password_confirmation').type('password')
       cy.get('button').click()
     })
-    cy.contains('Invalid login credentials. Please try again.')
+    cy.contains('Invalid signup credentials. Please try again.')
   })
 
   it('with invalid credentials - password confirmation failed', () => {
     cy.visit('http://localhost:3001');
+    cy.server()
+    cy.route({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/auth',
+      status: "401",
+      response: {
+        "errors": [
+          "Invalid signup credentials. Please try again."
+        ],
+        "success":false
+      }
+    })
     cy.get('#signup').click();
     cy.get('#signup-form').within(() => {
       cy.get('#email').type('another@mail.com')
@@ -32,7 +65,6 @@ describe('User can sign up', () => {
       cy.get('#password_confirmation').type('badword')
       cy.get('button').click()
     })
-    cy.contains('Invalid login credentials. Please try again.')
+    cy.contains('Invalid signup credentials. Please try again.')
   })
-
 })
