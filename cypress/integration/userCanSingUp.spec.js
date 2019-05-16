256b9102ad35be0f4059c54338042d6b50beb08b
zanenkn <zane.neikena@gmail.com>
2019-05-16 18:53:12 +0200

    Creates new feature test - user can sign up

---
+++ cypress/integration/userCanSingUp.spec.js
@@ -0,0 +1,38 @@
+describe('User can sign in', () => {
+  it('successfully', () => {
+    cy.visit('http://localhost:3001');
+    cy.get('#signup').click();
+    cy.get('#signup-form').within(() => {
+      cy.get('#email').type('another@mail.com')
+      cy.get('#password').type('password')
+      cy.get('#password-confirmation').type('password')
+      cy.get('button').click()
+    })
+    cy.contains('Hi another@mail.com')
+  })
+
+  it('with invalid credentials - invalid email', () => {
+    cy.visit('http://localhost:3001');
+    cy.get('#signup').click();
+    cy.get('#signup-form').within(() => {
+      cy.get('#email').type('another@mail')
+      cy.get('#password').type('password')
+      cy.get('#password-confirmation').type('password')
+      cy.get('button').click()
+    })
+    cy.contains('Invalid login credentials. Please try again.')
+  })
+
+  it('with invalid credentials - password confirmation failed', () => {
+    cy.visit('http://localhost:3001');
+    cy.get('#signup').click();
+    cy.get('#signup-form').within(() => {
+      cy.get('#email').type('another@mail.com')
+      cy.get('#password').type('password')
+      cy.get('#password-confirmation').type('badword')
+      cy.get('button').click()
+    })
+    cy.contains('Invalid login credentials. Please try again.')
+  })
+
+})
