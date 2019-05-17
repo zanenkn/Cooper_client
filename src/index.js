2e549ad439cfb9968d0930c3e91af4e179126fbd
zanenkn <zane.neikena@gmail.com>
2019-05-17 18:40:11 +0200

    CooperCalculator renders succesfully into the switch area

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
-import CooperCalculator from './Components/InputFields'
+import CooperCalculator from './Components/CooperCalculator'
 import DisplayPerformanceData from './Components/DisplayPerformanceData'
 import Header from './Components/Header'
 
